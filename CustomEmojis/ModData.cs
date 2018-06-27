2158f42bafc6903d9c55de548b5d3d8c56df46d5
FerMod <tudelaigarcia@gmail.com>
2018-06-27 14:54:24 +0200

    Rewrite files checksum code

--- CustomEmojis/ModData.cs
+++ CustomEmojis/ModData.cs
@@ -1,80 +1,173 @@
 ﻿
+using CustomEmojis;
 using CustomEmojis.Framework.Utilities;
-using MultiplayerEmotes;
-using StardewModdingAPI;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-class ModData {
+public class ModData {
 
 	public string ModFolder { get; set; }
 	public bool FilesChanged { get; set; }
 	public int EmojisAdded { get; set; }
-	public List<string> WatchedFiles { get; set; }
+	public List<string> WatchedPaths { get; set; }
 	public Dictionary<string, string> FilesChecksums { get; set; }
 
 	public ModData() {
 
 		FilesChanged = true;
-
-		WatchedFiles = new List<string> {
-			"vanillaEmojis.png",
-			"sprites"
-		};
-
+		EmojisAdded = 0;
+		WatchedPaths = new List<string>();
 		FilesChecksums = new Dictionary<string, string>();
 
-		EmojisAdded = 0;
-
 	}
 
-	public void UpdateFilesChecksum(string modFolder, string[] imageExtensions) {
+	//public void UpdateFilesChecksum(string modFolder, string[] fileExtensions) {
 
-		if (ModFolder != modFolder) {
-			ModFolder = modFolder;
-		}
+	//	if(ModFolder != modFolder) {
+	//		ModFolder = modFolder;
+	//	}
 
-		if (WatchedFiles != null) {
-			UpdateFilesChecksum(modFolder, imageExtensions, WatchedFiles);
-		}
-	}
+	//	if(WatchedPaths != null) {
+	//		UpdateFilesChecksum(modFolder, fileExtensions, WatchedPaths);
+	//	}
+	//}
 
-	public void UpdateFilesChecksum(string modFolder, string[] imageExtensions, List<string> watchedFiles) {
+	//public void UpdateFilesChecksum(string modFolder, string[] imageExtensions, List<string> watchedFiles, bool watchedSubfolder = false) {
 
-		foreach (string file in watchedFiles) {
+	//	foreach(string file in watchedFiles.ToList()) {
+	//		string absolutePath = Path.Combine(modFolder, file);
+	//		if(File.Exists(absolutePath) || Directory.Exists(absolutePath)) {
 
-			if (File.GetAttributes(Path.Combine(modFolder, file)).HasFlag(FileAttributes.Directory)) {
-				IEnumerable<string> files = ModUtilities.GetFiles(Path.Combine(modFolder, file), imageExtensions, SearchOption.AllDirectories);
-				UpdateFilesChecksum(modFolder, imageExtensions, files.ToList());
-			} else {
-				string filePath = Path.Combine(modFolder, file);
-				string relativePath = ModUtilities.GetRelativePath(modFolder, filePath);
-				if (File.Exists(filePath)) {
-					FilesChecksums[relativePath] = ModUtilities.CalculateFileHash(filePath);
-				}
-			}
+	//			if(File.GetAttributes(absolutePath).HasFlag(FileAttributes.Directory)) {
+	//				string relativePath = ModUtilities.GetRelativePath(modFolder, absolutePath);
+	//				if(WatchedPaths.Contains(relativePath)) {
+	//					IEnumerable<string> files = ModUtilities.GetFiles(Path.Combine(modFolder, file), imageExtensions, SearchOption.AllDirectories);
+	//					UpdateFilesChecksum(modFolder, imageExtensions, files.ToList(), true);
+	//				}
+	//			} else {
+	//				//FilesChecksums.Remove(relativePath);
+	//				string relativePath = ModUtilities.GetRelativePath(modFolder, file);
+	//				if(watchedSubfolder || WatchedPaths.Contains(relativePath)) {
+	//					FilesChecksums[relativePath] = ModUtilities.CalculateFileHash(absolutePath);
+	//				} else {
+	//					FilesChecksums.Remove(relativePath);
+	//				}
+	//			}
+	//		} else {
+	//			WatchedPaths.Remove(file);
+	//			string relativePath = ModUtilities.GetRelativePath(modFolder, absolutePath);
+	//			if(FilesChecksums.ContainsKey(relativePath)) {
+	//				FilesChecksums.Remove(relativePath);
+	//			}
+	//		}
+	//	}
 
-		}
+	//	FilesChanged = false;
+	//}
 
-	}
-
-	public bool Checksum() {
-		FilesChanged = false;
-
-		foreach (KeyValuePair<string, string> entry in FilesChecksums) {
-			string relativePath = Path.Combine(ModFolder, entry.Key);
-			if (!File.Exists(relativePath)) {
-				FilesChanged = true;
-			} else {
-				string key = ModUtilities.CalculateFileHash(relativePath);
-				if (!FilesChecksums.ContainsKey(key) || entry.Key != key) {
-					FilesChanged = true;
-				}
+	public bool Checksum(string[] imageExtensions) {
+		if(!FilesChanged) {
+			foreach(string path in WatchedPaths) {
+				FilesChanged = Checksum(path, imageExtensions);
 			}
 		}
 		return FilesChanged;
 	}
+
+	public bool Checksum(string path, string[] imageExtensions) {
+
+		string absolutePath = Path.Combine(ModFolder, path);
+
+		if(Directory.Exists(absolutePath)) {
+
+			IEnumerable<string> fileEnumeration = ModUtilities.GetFiles(absolutePath, imageExtensions, SearchOption.AllDirectories);
+			ModEntry.ModMonitor.Log($"Found Files: ");
+			foreach(var item in fileEnumeration.ToList()) {
+				ModEntry.ModMonitor.Log($"{item}");
+			}
+			IEnumerable<string> differentValues = FilesChecksums.Keys.ToList().Except(fileEnumeration.Select(x => ModUtilities.GetRelativePath(ModFolder, x)));
+			ModEntry.ModMonitor.Log($"For each different value:");
+			foreach(string key in differentValues.ToList()) {
+				ModEntry.ModMonitor.Log($"{key}");
+				if(FilesChecksums.ContainsKey(key)) {
+					FilesChecksums.Remove(key);
+				}
+			}
+
+			foreach(string file in fileEnumeration.ToList()) {
+				string relativePath = ModUtilities.GetRelativePath(ModFolder, file);
+				FilesChanged = Checksum(relativePath, imageExtensions);
+			}
+
+		} else if(File.Exists(absolutePath)) {
+
+			string fileHash = ModUtilities.CalculateFileHash(absolutePath);
+
+			string key;
+			if(FilesChecksums.ContainsValue(fileHash)) {
+				key = FilesChecksums.FirstOrDefault(x => x.Value == fileHash).Key;
+				if(path != key) {
+					FilesChecksums.Remove(key);
+				}
+			}
+
+			if(FilesChecksums.TryGetValue(path, out string hash)) {
+
+				if(fileHash != hash) {
+					FilesChecksums[path] = fileHash;
+					FilesChanged = true;
+				}
+
+			} else {
+				FilesChecksums[path] = fileHash;
+				FilesChanged = true;
+			}
+
+		} else {
+
+			string relativePath = ModUtilities.GetRelativePath(ModFolder, absolutePath);
+			if(FilesChecksums.ContainsKey(relativePath)) {
+				FilesChecksums.Remove(relativePath);
+				FilesChanged = true;
+			}
+
+		}
+
+		return FilesChanged;
+	}
+
+	//public bool Checksum() {
+
+	//	for(int i = 0; i < WatchedPaths.Count; i++) {
+	//		KeyValuePair<string, string> entry = FilesChecksums.ElementAt(i);
+	//		IEnumerable<string> files = ModUtilities.GetFiles(Path.Combine(ModFolder, entry.Key), WatchedFilesExtension, SearchOption.AllDirectories);
+	//		foreach(string file in files) {
+	//			string absolutePath = Path.Combine(ModFolder, file);
+	//			string relativePath = ModUtilities.GetRelativePath(ModFolder, absolutePath);
+	//			if(WatchedPaths.Contains(relativePath)) {
+	//			}
+	//		}
+	//	}
+	//	/*
+	//	for(int i = 0; i < FilesChecksums.Count; i++) {
+	//		KeyValuePair<string, string> entry = FilesChecksums.ElementAt(i);
+	//		string absolutePath = Path.Combine(ModFolder, entry.Key);
+	//		if(!File.Exists(absolutePath)) {
+	//			FilesChanged = true;
+	//			FilesChecksums.Remove(entry.Key);
+	//		} else {
+	//			string fileHash = ModUtilities.CalculateFileHash(absolutePath);
+	//			string relativePath = ModUtilities.GetRelativePath(ModFolder, absolutePath);
+	//			if(entry.Value != fileHash || !WatchedFiles.Contains(relativePath)) {
+	//				FilesChanged = true;
+	//				FilesChecksums[entry.Key] = fileHash;
+	//			}
+	//		}
+
+	//	}
+	//	*/
+	//	return FilesChanged;
+	//}
 
 }
--- CustomEmojis/ModEntry.cs
+++ CustomEmojis/ModEntry.cs
@@ -17,6 +17,11 @@ using CustomEmojis.Framework.Patches;
 using CustomEmojis.Framework;
 using System.IO;
 using CustomEmojis.Patches;
+using System.Collections.Generic;
+using CustomEmojis.Framework.Constants;
+using CustomEmojis.Framework.Overrides;
+using CustomEmojis.Framework.Utilities;
+using CustomEmojis.Framework.Benchmark;
 
 namespace CustomEmojis {
 
@@ -28,6 +33,8 @@ namespace CustomEmojis {
 
 		// TODO: Remove. Used for debugging
 		public static IMonitor ModMonitor { get; private set; }
+
+		CustomEmojiDrawer ced;
 
 #if(DEBUG)
 		private ModDebugData modDebugData;
@@ -46,57 +53,93 @@ namespace CustomEmojis {
 			this.Monitor.Log("Loading mod data...", LogLevel.Trace);
 			this.modData = this.Helper.ReadJsonFile<ModData>("data.json");
 
+			Monitor.Log($"[ModEntry] Timer started!", LogLevel.Trace);
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
 			if(this.modData == null) {
-				this.Monitor.Log("Mod data file not found. (harmless info)", StardewModdingAPI.LogLevel.Trace);
-				this.modData = new ModData();
+				this.Monitor.Log("Mod data file not found. (harmless info)", LogLevel.Trace);
+				this.modData = new ModData {
+					ModFolder = this.Helper.DirectoryPath,
+					WatchedPaths = new List<string>() {
+						Assets.InputFolder
+					}
+				};
 			} else {
 				this.Monitor.Log("Making checksum...", LogLevel.Trace);
-				this.modData.Checksum();
+				this.modData.Checksum(config.ImageExtensions);
+				this.Helper.WriteJsonFile("data.json", modData);
 			}
+			sw.Stop();
+			Monitor.Log($"[ModEntry] Timer Stoped! Elapsed time: {sw.Elapsed}");
 
-#if(DEBUG)
+#if(!DEBUG)
 			this.Monitor.Log("Loading debug data file...", LogLevel.Trace);
 			this.modDebugData = this.Helper.ReadJsonFile<ModDebugData>("debugData.json") ?? new ModDebugData();
 
 			if(modDebugData.ActAsHost()) {
 				this.Helper.WriteJsonFile("debugData.json", modDebugData);
 				Monitor.Log($"====> HOST <====");
-				emojiAssetsLoader = new EmojiAssetsLoader(helper, EmojiMenu.EMOJI_SIZE, this.config.ImageExtensions, this.modData.FilesChanged) {
-					VanillaEmojisTexture = helper.Content.Load<Texture2D>("LooseSprites\\emojis", ContentSource.GameContent)
-				};
+				emojiAssetsLoader = new EmojiAssetsLoader(helper, EmojiMenu.EMOJI_SIZE, this.config.ImageExtensions, this.modData.FilesChanged);
 			} else {
 				this.Helper.WriteJsonFile("debugData.json", modDebugData);
 				Monitor.Log($"====> CLIENT <====");
-				emojiAssetsLoader = new EmojiAssetsLoader(helper, EmojiMenu.EMOJI_SIZE, this.config.ImageExtensions, true) {
-					VanillaEmojisTexture = helper.Content.Load<Texture2D>("LooseSprites\\emojis", ContentSource.GameContent),
-					InputFolder = "spritesCLIENT",
-					OutputFolder = "mergedSpriteCLIENT"
-				};
+				Assets.InputFolder = Assets.InputFolder + "CLIENT";
+				Assets.OutputFolder = Assets.OutputFile + "CLIENT";
+				emojiAssetsLoader = new EmojiAssetsLoader(helper, EmojiMenu.EMOJI_SIZE, this.config.ImageExtensions, true);
 			}
 #else
-			emojiAssetsLoader = new EmojiAssetsLoader(helper, EmojiMenu.EMOJI_SIZE, this.config.ImageExtensions, this.modData.FilesChanged) {
-				VanillaEmojisTexture = helper.Content.Load<Texture2D>("LooseSprites\\emojis", ContentSource.GameContent)
-			};
+			emojiAssetsLoader = new EmojiAssetsLoader(helper, EmojiMenu.EMOJI_SIZE, this.config.ImageExtensions, this.modData.FilesChanged, true);
 #endif
 
 			helper.Content.AssetLoaders.Add(emojiAssetsLoader);
+
+			helper.ConsoleCommands.Add("reload_emojis", "Reload the game emojis with the new ones found in the mod folder.", this.ReloadEmojis);
 
 			SaveEvents.AfterLoad += this.OnAfterLoad;
 
 			MultiplayerExtension.OnRecieveEmojiTexture += MultiplayerExtension_OnRecieveEmojiTexture;
 			MultiplayerExtension.OnRecieveEmojiTextureRequest += MultiplayerExtension_OnRecieveEmojiTextureRequest;
+
+			GraphicsEvents.OnPostRenderEvent += GraphicsEvents_OnPostRenderEvent;
+			GameEvents.FirstUpdateTick += GameEvents_FirstUpdateTick;
+		}
+
+		private void GameEvents_UpdateTick(object sender, EventArgs e) {
+			if(Context.IsWorldReady && Game1.chatBox != null) {
+				List<ChatMessage> messages = Helper.Reflection.GetField<List<ChatMessage>>(Game1.chatBox, "messages").GetValue();
+				int num = 0;
+				for(int i = messages.Count - 1; i >= 0; --i) {
+					ChatMessage message = messages[i];
+					num += message.verticalSize;
+					//message.draw(b, 12, this.yPositionOnScreen - num - 8 + (this.chatBox.Selected ? 0 : this.chatBox.Height));
+					Monitor.Log($"Draw custom emojis");
+				}
+				//ced = new CustomEmojiDrawer(Helper.Reflection, emojiAssetsLoader.VanillaEmojisTexture, EmojiMenu.totalEmojis);// emojiAssetsLoader.CustomEmojisTexture);
+			}
+		}
+
+		private void GameEvents_FirstUpdateTick(object sender, EventArgs e) {
+
+		}
+
+		private void GraphicsEvents_OnPostRenderEvent(object sender, EventArgs e) {
+			//if(Context.IsWorldReady) {
+			//	if(ced == null) {
+			//		ced = new CustomEmojiDrawer(Helper.Reflection, emojiAssetsLoader.VanillaEmojisTexture, EmojiMenu.totalEmojis);// emojiAssetsLoader.CustomEmojisTexture);
+			//	}
+			//}
 		}
 
 		private void MultiplayerExtension_OnRecieveEmojiTexture(object sender, RecievedEmojiTextureEventArgs e) {
 			emojiAssetsLoader.CustomEmojisTexture = e.EmojiTexture;
-			Helper.Reflection.GetField<Texture2D>(Game1.chatBox.emojiMenu, "emojiTexture").SetValue(e.EmojiTexture);
-			ChatBox.emojiTexture = e.EmojiTexture;
-			EmojiMenu.totalEmojis = e.NumberEmojis;
+			//Helper.Reflection.GetField<Texture2D>(Game1.chatBox.emojiMenu, "emojiTexture").SetValue(e.EmojiTexture);
+			//ChatBox.emojiTexture = e.EmojiTexture;
+			//EmojiMenu.totalEmojis = e.NumberEmojis;
 			//Monitor.Log($"AssetKey: {Helper.Content.GetActualAssetKey(Path.Combine(emojiAssetsLoader.OutputFolder, emojiAssetsLoader.OutputFile))}");
 			//string assetKey = Helper.Content.GetActualAssetKey(Path.Combine(emojiAssetsLoader.OutputFolder, emojiAssetsLoader.OutputFile));
 			//Helper.Content.InvalidateCache(assetKey);
 			//Helper.Content.InvalidateCache(asset => asset.DataType == typeof(Texture2D) && asset.AssetNameEquals(assetKey));
-			//Helper.Content.InvalidateCache(@"LooseSprites\emojis.xnb");
+			Helper.Content.InvalidateCache("LooseSprites/emojis");
 			//Helper.Content.InvalidateCache(asset => asset.DataType == typeof(Texture2D) && asset.AssetNameEquals(@"LooseSprites\emojis.xnb"));
 		}
 
@@ -114,51 +157,101 @@ namespace CustomEmojis {
 		/// <param name="sender">The event sender.</param>
 		/// <param name="e">The event arguments.</param>
 		private void OnAfterLoad(object sender, EventArgs e) {
+			if(Game1.chatBox != null) {
+				// Check if is exact type
+				//if(Game1.chatBox.GetType() == typeof(ChatBox)) {
+				//string typeName = "System.String";
+				//Type typeArgument = Type.GetType(Game1.chatBox.GetType().FullName);
 
+				//Type genericClass = typeof(ChatBoxOverride<>);
+				//// MakeGenericType is badly named
+				//Type constructedClass = genericClass.MakeGenericType(Game1.chatBox.GetType());
+
+				//object created = Activator.CreateInstance(constructedClass);
+
+				//Type type = typeof(Game1).MakeGenericType(Game1.chatBox.GetType());
+				//var context = Activator.CreateInstance(type);
+				//object o = Activator.CreateInstance(constructedClass);
+				//chatBoxOverride = ModUtilities.Construct<ChatBoxOverride<>>(typeof(ChatBox));
+				//Game1.chatBox = chatBoxOverride;
+				//Game1.chatBox = ModUtilities.Construct<ChatBox, ChatBoxOverride>(Game1.chatBox);
+				//var chatbox = ModUtilities.Construct<ChatBoxOverride>(typeof(ChatBox), Game1.chatBox);
+				//Game1.chatBox = chatbox;
+				//} else {
+				//	Monitor.Log($"[{Game1.chatBox}]: This isn't ChatBox Type :(");
+				//}
+			}
 #if(DEBUG)
 			// Pause time and set it to 09:00
 			Helper.ConsoleCommands.Trigger("world_freezetime", new string[] { "1" });
 			Helper.ConsoleCommands.Trigger("world_settime", new string[] { "0900" });
 #endif
 
-
-			if(modData.FilesChanged) {
+			Stopwatch sw = new Stopwatch();
+			Monitor.Log($"Timer started!");
+			sw.Start();
+			//modData.UpdateFilesChecksum(this.Helper.DirectoryPath, config.ImageExtensions);
+			if(modData.Checksum(config.ImageExtensions)) {
 				this.Monitor.Log("File changes detected. Saving mod data...", LogLevel.Trace);
-				modData.EmojisAdded = emojiAssetsLoader.NumberCustomEmojisAdded;
-				modData.UpdateFilesChecksum(this.Helper.DirectoryPath, config.ImageExtensions);
+				modData.EmojisAdded = emojiAssetsLoader.UpdateTotalEmojis();
 				modData.FilesChanged = false;
 				this.Helper.WriteJsonFile("data.json", modData);
 			} else {
 				this.Monitor.Log("No file changes detected.", LogLevel.Trace);
 				emojiAssetsLoader.NumberCustomEmojisAdded = modData.EmojisAdded;
 			}
+			Monitor.Log($"[After checksum] Time elapsed: {sw.Elapsed}");
+			sw.Reset();
 
 			this.Monitor.Log($"Custom emojis added: {emojiAssetsLoader.CustomEmojisAdded}");
 			if(emojiAssetsLoader.CustomEmojisAdded) {
 				this.Monitor.Log($"Custom emojis found: {emojiAssetsLoader.NumberCustomEmojisAdded}");
 				this.Monitor.Log($"Total emojis counted by Stardew Valley: {EmojiMenu.totalEmojis}");
-				//int a = (int)Math.Ceiling((double)emojiAssetsLoader.NumberEmojisAdded / 14);
-				//int emojiPerRow = (emojiAssetsLoader.VanillaEmojisTexture.Width / emojiAssetsLoader.EmojisSize);
-				//int numberCustomEmojis = (emojiAssetsLoader.NumberEmojisAdded - emojiPerRow * a);
-				//EmojiMenu.totalEmojis += numberCustomEmojis;
-				EmojiMenu.totalEmojis += (emojiAssetsLoader.NumberCustomEmojisAdded - ((emojiAssetsLoader.VanillaEmojisTexture.Width / emojiAssetsLoader.EmojisSize) * (int)Math.Ceiling((double)emojiAssetsLoader.NumberCustomEmojisAdded / 14)));
-				this.Monitor.Log($"Total emojis counted after ammount fix: {EmojiMenu.totalEmojis}");
-
+				this.Monitor.Log($"Total emojis counted after ammount fix: {emojiAssetsLoader.TotalNumberEmojis}");
 			}
+			//if(emojiAssetsLoader.CustomEmojisAdded) {
+			//	this.Monitor.Log($"Custom emojis found: {emojiAssetsLoader.NumberCustomEmojisAdded}");
+			//	this.Monitor.Log($"Total emojis counted by Stardew Valley: {EmojiMenu.totalEmojis}");
+			//	//int a = (int)Math.Ceiling((double)emojiAssetsLoader.NumberEmojisAdded / 14);
+			//	//int emojiPerRow = (emojiAssetsLoader.VanillaEmojisTexture.Width / emojiAssetsLoader.EmojisSize);
+			//	//int numberCustomEmojis = (emojiAssetsLoader.NumberEmojisAdded - emojiPerRow * a);
+			//	//EmojiMenu.totalEmojis += numberCustomEmojis;
+			//	emojiAssetsLoader.UpdateTotalEmojis();
+			//	this.Monitor.Log($"Total emojis counted after ammount fix: {emojiAssetsLoader.TotalNumberEmojis}");
+
+			//}
+			Monitor.Log($"[After UpdateTotalEmojis]Time elapsed: {sw.Elapsed}");
+			sw.Reset();
+
 
 			if(!Context.IsMainPlayer && emojiAssetsLoader.CustomEmojisAdded) {
 				Multiplayer multiplayer = this.Helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue();
 				multiplayer.RequestEmojiTexture();
 			}
-
+			sw.Stop();
+			Monitor.Log($"[After Request] Time elapsed: {sw.Elapsed}");
 		}
 
-		/// <summary>Reload the game emojis with new ones found in the 'CustomEmojis\sprites\' folder when the 'reloadEmojis' command is invoked.</summary>
+		/// <summary>Reload the game emojis with the new ones found in the mod folder.</summary>
 		/// <param name="command">The name of the command invoked.</param>
 		/// <param name="args">The arguments received by the command. Each word after the command name is a separate argument.</param>
 		private void ReloadEmojis(string command, string[] args) {
 			this.Monitor.Log($"Reloading emoji assets...");
-			this.Helper.Content.InvalidateCache(asset => asset.DataType == typeof(Texture2D) && asset.AssetNameEquals(@"LooseSprites\\emojis.xnb"));
+
+			Assets.InputFolder = "CustomAssetsCLIENT";
+
+			bool cacheInvalidated = Helper.Content.InvalidateCache("LooseSprites/emojis");
+			//bool cacheInvalidated = Helper.Content.InvalidateCache(Helper.Content.GetActualAssetKey($"{Assets.OutputFolder}/{Assets.OutputFile}"));
+			emojiAssetsLoader.UpdateTotalEmojis();
+			//bool invalidated = Helper.Content.InvalidateCache(asset => asset.DataType == typeof(Texture2D) && asset.AssetNameEquals(@"emojis"));
+			this.Monitor.Log($"CacheInvalidated: {cacheInvalidated}");
+			Helper.Reflection.GetField<Texture2D>(Game1.chatBox.emojiMenu, "emojiTexture").SetValue(Helper.Content.Load<Texture2D>($"{Assets.OutputFolder}/{Assets.OutputFile}"));
+			ChatBox.emojiTexture = Helper.Content.Load<Texture2D>($"{Assets.OutputFolder}/{Assets.OutputFile}");
+			//Helper.Content.Load<Texture2D>($"{Assets.OutputFolder}/{Assets.OutputFile}", ContentSource.ModFolder);
+
+			//Helper.Content.InvalidateCache<Texture2D>();
+
+			//this.Helper.Content.InvalidateCache(asset => asset.DataType == typeof(Texture2D) && asset.AssetNameEquals(@"LooseSprites\\emojis.xnb"));
 		}
 
 	}
