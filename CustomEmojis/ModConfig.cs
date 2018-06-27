b178f97145160421a7e9c42d2ab54bddc7d7c663
FerMod <tudelaigarcia@gmail.com>
2018-06-27 14:53:33 +0200

    Remove to the user the ability choose output and input folders

--- CustomEmojis/ModConfig.cs
+++ CustomEmojis/ModConfig.cs
@@ -1,13 +1,15 @@
 ﻿
-using StardewValley.Menus;
-
-class ModConfig {
+public class ModConfig {
 
 	//public int EmojisSize { get; set; }
 	public string[] ImageExtensions { get; set; }
+	public string InputFolder { get; set; }
+	public string OutputFolder { get; set; }
 
 	public ModConfig() {
 		//this.EmojisSize = EmojiMenu.EMOJI_SIZE;
 		this.ImageExtensions = new string[] { ".png", ".jpg", ".jpeg", ".gif"};
+		this.InputFolder = "sprites";
+		this.OutputFolder = "mergedSprite";
 	}
 }
