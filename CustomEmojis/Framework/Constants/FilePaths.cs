5883c1e353f4c211d78c85de42a5f7e2eb5568c2
FerMod <tudelaigarcia@gmail.com>
2018-06-29 19:41:27 +0200

    Add new constant

---
+++ CustomEmojis/Framework/Constants/FilePaths.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEmojis.Framework.Constants {
+
+	internal static class FilePaths {
+		internal const string Data = "data.json";
+	}
+
+}
