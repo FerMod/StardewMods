9d58d174b0b6a14c5ba5dea8ea87322303d7d34b
FerMod <tudelaigarcia@gmail.com>
2018-06-30 13:22:30 +0200

    Cleanup class

--- CustomEmojis/ModData.cs
+++ CustomEmojis/ModData.cs
@@ -35,50 +35,6 @@ public class ModData {
 		return FilesChanged;
 	}
 
-	//public void UpdateFilesChecksum(string modFolder, string[] fileExtensions) {
-
-	//	if(ModFolder != modFolder) {
-	//		ModFolder = modFolder;
-	//	}
-
-	//	if(WatchedPaths != null) {
-	//		UpdateFilesChecksum(modFolder, fileExtensions, WatchedPaths);
-	//	}
-	//}
-
-	//public void UpdateFilesChecksum(string modFolder, string[] imageExtensions, List<string> watchedFiles, bool watchedSubfolder = false) {
-
-	//	foreach(string file in watchedFiles.ToList()) {
-	//		string absolutePath = Path.Combine(modFolder, file);
-	//		if(File.Exists(absolutePath) || Directory.Exists(absolutePath)) {
-
-	//			if(File.GetAttributes(absolutePath).HasFlag(FileAttributes.Directory)) {
-	//				string relativePath = ModUtilities.GetRelativePath(modFolder, absolutePath);
-	//				if(WatchedPaths.Contains(relativePath)) {
-	//					IEnumerable<string> files = ModUtilities.GetFiles(Path.Combine(modFolder, file), imageExtensions, SearchOption.AllDirectories);
-	//					UpdateFilesChecksum(modFolder, imageExtensions, files.ToList(), true);
-	//				}
-	//			} else {
-	//				//FilesChecksums.Remove(relativePath);
-	//				string relativePath = ModUtilities.GetRelativePath(modFolder, file);
-	//				if(watchedSubfolder || WatchedPaths.Contains(relativePath)) {
-	//					FilesChecksums[relativePath] = ModUtilities.CalculateFileHash(absolutePath);
-	//				} else {
-	//					FilesChecksums.Remove(relativePath);
-	//				}
-	//			}
-	//		} else {
-	//			WatchedPaths.Remove(file);
-	//			string relativePath = ModUtilities.GetRelativePath(modFolder, absolutePath);
-	//			if(FilesChecksums.ContainsKey(relativePath)) {
-	//				FilesChecksums.Remove(relativePath);
-	//			}
-	//		}
-	//	}
-
-	//	FilesChanged = false;
-	//}
-
 	public bool Checksum(string[] imageExtensions) {
 		if(!FilesChanged) {
 			foreach(string path in WatchedPaths) {
@@ -119,7 +75,7 @@ public class ModData {
 		} else if(File.Exists(absolutePath)) {
 
 			string fileHash = ModUtilities.CalculateFileHash(absolutePath);
-
+			
 			// Check if the calculated hash is already associated with a file path
 			if(FilesChecksums.TryGetBySecond(fileHash, out string filePath)) {
 				// If is different, remove the outdated path
@@ -153,38 +109,5 @@ public class ModData {
 
 		return FilesChanged;
 	}
-
-	//public bool Checksum() {
-
-	//	for(int i = 0; i < WatchedPaths.Count; i++) {
-	//		KeyValuePair<string, string> entry = FilesChecksums.ElementAt(i);
-	//		IEnumerable<string> files = ModUtilities.GetFiles(Path.Combine(ModFolder, entry.Key), WatchedFilesExtension, SearchOption.AllDirectories);
-	//		foreach(string file in files) {
-	//			string absolutePath = Path.Combine(ModFolder, file);
-	//			string relativePath = ModUtilities.GetRelativePath(ModFolder, absolutePath);
-	//			if(WatchedPaths.Contains(relativePath)) {
-	//			}
-	//		}
-	//	}
-	//	/*
-	//	for(int i = 0; i < FilesChecksums.Count; i++) {
-	//		KeyValuePair<string, string> entry = FilesChecksums.ElementAt(i);
-	//		string absolutePath = Path.Combine(ModFolder, entry.Key);
-	//		if(!File.Exists(absolutePath)) {
-	//			FilesChanged = true;
-	//			FilesChecksums.Remove(entry.Key);
-	//		} else {
-	//			string fileHash = ModUtilities.CalculateFileHash(absolutePath);
-	//			string relativePath = ModUtilities.GetRelativePath(ModFolder, absolutePath);
-	//			if(entry.Value != fileHash || !WatchedFiles.Contains(relativePath)) {
-	//				FilesChanged = true;
-	//				FilesChecksums[entry.Key] = fileHash;
-	//			}
-	//		}
-
-	//	}
-	//	*/
-	//	return FilesChanged;
-	//}
 
 }
