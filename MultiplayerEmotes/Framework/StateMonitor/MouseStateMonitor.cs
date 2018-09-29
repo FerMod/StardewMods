3fd1b8a98f2aeb5b9e4a46f6a05c3e2c5701c589
FerMod <tudelaigarcia@gmail.com>
2018-09-29 18:32:48 +0200

    Change method condition

--- MultiplayerEmotes/Framework/StateMonitor/MouseStateMonitor.cs
+++ MultiplayerEmotes/Framework/StateMonitor/MouseStateMonitor.cs
@@ -1,14 +1,6 @@
 ﻿
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-using StardewModdingAPI;
-using StardewModdingAPI.Events;
-using StardewValley;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
 
 namespace MultiplayerEmotes.Events {
 
@@ -60,7 +52,7 @@ namespace MultiplayerEmotes.Events {
 		}
 
 		public static bool ScrollChanged() {
-			return ScrollIncreased() || ScrollDecreased();
+			return CurrentMouseState.ScrollWheelValue != PreviousMouseState.ScrollWheelValue;
 		}
 	}
 
