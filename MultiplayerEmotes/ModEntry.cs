eec051124a83808772d3bedc1ab36c956833dc7a
FerMod <tudelaigarcia@gmail.com>
2019-03-05 18:40:03 +0100

    Fix typo and doc style

--- MultiplayerEmotes/ModEntry.cs
+++ MultiplayerEmotes/ModEntry.cs
@@ -1,5 +1,5 @@
 
-using System;
+using System.Text;
 using MultiplayerEmotes.Framework;
 using MultiplayerEmotes.Framework.Network;
 using MultiplayerEmotes.Framework.Patches;
@@ -22,9 +22,8 @@ namespace MultiplayerEmotes {
 		public static IModHelper ModHelper { get; private set; }
 
 		/*
-		 Emotes only visible by others with the mod.
-		 The host needs to have the mod, to others with the mod use it.
-		 If the host does not have the mod, it will not work.
+		 * NOTE:
+		 * Emotes are only visible by other players with the mod installed.
 		 */
 		public override void Entry(IModHelper helper) {
 
@@ -47,8 +46,10 @@ namespace MultiplayerEmotes {
 
 			// TODO: Command to stop emotes from NPC and FarmAnimals
 			helper.ConsoleCommands.Add("emote", "Play the emote animation with the passed id.\n\nUsage: emote <value>\n- value: a integer representing the animation id.", this.Emote);
+			helper.ConsoleCommands.Add("emote_npc", "Force a npc to play the emote animation with the given id.\n\nUsage: emote_npc <value> <npcName>\n- value: a integer representing the animation id.\n- npcName: a string representing the npc name.", this.EmoteNpc);
 			helper.ConsoleCommands.Add("stop_emote", "Stop any emote being played by you.\n\nUsage: stop_emote", this.StopEmote);
 			helper.ConsoleCommands.Add("stop_all_emotes", "Stop any emote being played.\n\nUsage: stop_all_emotes", this.StopAllEmotes);
+			helper.ConsoleCommands.Add("multiplayer_emotes", "List all the players that have this mod and can send and receive emotes.\n\nUsage: multiplayer_emotes", this.MultiplayerEmotesAvailable);
 
 		}
 
@@ -66,33 +67,61 @@ namespace MultiplayerEmotes {
 			Game1.onScreenMenus.Insert(0, emoteMenuButton);
 
 #if DEBUG
-			// Pause time and set it to 09:00
-			Helper.ConsoleCommands.Trigger("world_freezetime", new string[] { "1" });
-			Helper.ConsoleCommands.Trigger("world_settime", new string[] { "0900" });
+			// Initialize and setup debug world
+			SetupDebugWorld();
 #endif
+
 		}
 
 		private void Emote(string command, string[] args) {
 
-			if(args.Length > 0) {
-
-				if(int.TryParse(args[0], out int id)) {
-					if(id > 0) {
-						Game1.player.doEmote(id * 4);
-						this.Monitor.Log($"Playing emote: {id}");
-#if DEBUG
-					} else if(id < 0) {
-						EmoteTemporaryAnimation emoteTempAnim = new EmoteTemporaryAnimation(Helper.Reflection, Helper.Events);
-						emoteTempAnim.BroadcastEmote(id * -1);
-						this.Monitor.Log($"Playing emote (workarround): {id * -1}");
-#endif
-					} else {
-						this.Monitor.Log($"The emote id value must be grater than 0.");
-					}
-				}
-
-			} else {
+			if(args.Length < 1) {
 				this.Monitor.Log($"Missing parameters.\n\nUsage: emote <value>\n- value: a integer representing the emote id.");
+				return;
+			}
+
+			if(!int.TryParse(args[0], out int id)) {
+				this.Monitor.Log($"The emote id must be a integer.");
+				return;
+			}
+
+			if(id > 0) {
+				Game1.player.doEmote(id * 4);
+				this.Monitor.Log($"Playing emote: {id}");
+#if DEBUG
+			} else if(id < 0) {
+				EmoteTemporaryAnimation emoteTempAnim = new EmoteTemporaryAnimation(Helper.Reflection, Helper.Events);
+				emoteTempAnim.BroadcastEmote(id * -1);
+				this.Monitor.Log($"Playing emote (workarround): {id * -1}");
+#endif
+			} else {
+				this.Monitor.Log($"The emote id value must be greater than 0.");
+			}
+
+		}
+
+		private void EmoteNpc(string command, string[] args) {
+
+			if(args.Length < 2) {
+				this.Monitor.Log($"Missing parameters.\n\nUsage: emote_npc <value> <npcName>\n- value: a integer representing the animation id.\n- npcName: a string representing the npc name.");
+				return;
+			}
+
+			if(!int.TryParse(args[0], out int id)) {
+				this.Monitor.Log($"The emote id must be a integer.");
+				return;
+			}
+
+			if(id > 0) {
+				NPC npc = Game1.getCharacterFromName(args[1]);
+				if(npc != null) {
+					npc.doEmote(id * 4);
+					this.Monitor.Log($"[id: {npc.id}, name: \"{npc.Name}\"] Playing emote: {id}");
+				} else {
+					this.Monitor.Log($"Could not find the NPC with the name \"{args[1]}\".");
+				}
+			} else {
+				this.Monitor.Log($"The emote id value must be greater than 0.");
 			}
 
 		}
@@ -110,14 +139,101 @@ namespace MultiplayerEmotes {
 
 		private void StopAllEmotes(string command, string[] args) {
 
-			this.Monitor.Log($"Stoping any player emotes...");
+			this.Monitor.Log($"Stoping any playing emotes...");
 			foreach(Farmer farmer in Game1.getAllFarmers()) {
-				if(farmer.IsEmoting) {
-					farmer.IsEmoting = false;
+				farmer.IsEmoting = false;
+			}
+
+			foreach(GameLocation location in Game1.locations) {
+				foreach(NPC npc in location.getCharacters()) {
+					npc.IsEmoting = false;
 				}
 			}
 
+			foreach(FarmAnimal farmAnimal in Game1.getFarm().getAllFarmAnimals()) {
+				farmAnimal.IsEmoting = false;
+			}
+
 		}
+
+		private void MultiplayerEmotesAvailable(string command, string[] args) {
+
+			if(!Context.IsMultiplayer) {
+				this.Monitor.Log($"You are not currently in a online session.", LogLevel.Info);
+				return;
+			}
+
+			// Number of players excluding the host
+			int numPlayers = Game1.getOnlineFarmers().Count - 1;
+
+			if(numPlayers <= 0) {
+				this.Monitor.Log($"No players connected in the current session.", LogLevel.Info);
+				return;
+			}
+
+			int playersWithMod = 0;
+			StringBuilder sb = new StringBuilder();
+
+			foreach(Farmer farmer in Game1.getOnlineFarmers()) {
+
+				// Check that is not the current player
+				if(Game1.player.UniqueMultiplayerID != farmer.UniqueMultiplayerID) {
+
+					IMultiplayerPeer peer = this.Helper.Multiplayer.GetConnectedPlayer(farmer.UniqueMultiplayerID);
+
+					if(peer.HasSmapi && peer.GetMod(this.ModManifest.UniqueID) != null) {
+						playersWithMod++;
+						sb.Append($"{playersWithMod}: \"{Game1.getFarmer(peer.PlayerID).Name}\"\n");
+					}
+
+				}
+
+			}
+
+			if(playersWithMod > 0) {
+				this.Monitor.Log($"From {numPlayers} player(s), {playersWithMod} have this mod:\n", LogLevel.Info);
+			} else {
+				this.Monitor.Log($"From {numPlayers} player(s), none has this mod.", LogLevel.Info);
+			}
+
+		}
+
+#if DEBUG
+		private void SetupDebugWorld() {
+
+			Game1.game1.parseDebugInput("zoomLevel 40");
+
+			if(Context.IsMainPlayer) {
+
+				// Pause time and set it to 09:00
+				Helper.ConsoleCommands.Trigger("world_freezetime", new string[] { "1" });
+				Helper.ConsoleCommands.Trigger("world_settime", new string[] { "0900" });
+
+				Game1.game1.parseDebugInput("warp Farm 64 15");
+
+				/*
+				Game1.game1.parseDebugInput("pet 64 15");
+				Game1.game1.parseDebugInput("petToFarm");
+				
+				//Game1.game1.parseDebugInput("setUpBigFarm");
+				Game1.game1.parseDebugInput("setUpFarm");
+
+				// Coop Animals
+				Game1.game1.parseDebugInput("animal Chicken");
+				Game1.game1.parseDebugInput("animal Duck");
+				Game1.game1.parseDebugInput("animal Rabbit");
+				Game1.game1.parseDebugInput("animal Dinosaur");
+
+				// Barn Animals
+				Game1.game1.parseDebugInput("animal Cow");
+				Game1.game1.parseDebugInput("animal Goat");
+				Game1.game1.parseDebugInput("animal Sheep");
+				Game1.game1.parseDebugInput("animal Pig");
+				*/
+			}
+
+		}
+#endif
 
 	}
 
--- MultiplayerEmotes/README.md
+++ MultiplayerEmotes/README.md
@@ -8,9 +8,9 @@ This interface is a button, that by left clicking it, opens and closes all the a
 This mod have been tested more heavily in Windows, and in less degree in Unix and Mac.
 If some type of issue related to this mod arises, please report it, so I can fix it.
 
-To know what changed between versions you can go to the [Release Notes](https://github.com/FerMod/StardewMods/tree/master/MultiplayerEmotes/release-notes.md).
+To know what changed between versions you can go to the [Release Notes](/MultiplayerEmotes/release-notes.md).
 
->**Note:** In order to see other player emotes they will also need to have this mod. Sorry, this is due to some limitations.
+> **Note:** In order to see other player emotes they will also need to have this mod. Sorry, this is due to some limitations.
 
 ## Instalation
 
@@ -19,27 +19,32 @@ To download the mod, go to the the page of [Multiplayer Emotes](https://www.nexu
 This mod requires [SMAPI](https://smapi.io/). All the help to install SMAPI and the troubleshooting help can be found in the "Player guide".
 
 Follow these spteps to install the mod:
+
 1. Download the mod [here](https://www.nexusmods.com/stardewvalley/mods/2347)
 2. Extract the `.zip` in the `Mods` folder
 
 ## Configuration File
-The mod allows  some configuration, and can be changed in the `config.json` file if you want. 
+
+The mod allows  some configuration, and can be changed in the `config.json` file if you want.
 The config file is generated once Stardew Valley is launched at least once with the mod installed.
 
 Available settings:
 
-| Setting                     | Effect
-| --------------------------- | ----------------------------------------------------------------------------------
-| `AnimateEmoteButtonIcon`    | Default `true`. Enable or disable the emote menu button animation
-| `ShowTooltipOnHover`        | Default `true`. Enable or disable the tooltip when hovering the emote menu button
+| Setting                  | Effect                                                                            |
+| ------------------------ | --------------------------------------------------------------------------------- |
+| `AnimateEmoteButtonIcon` | Default `true`. Enable or disable the emote menu button animation                 |
+| `ShowTooltipOnHover`     | Default `true`. Enable or disable the tooltip when hovering the emote menu button |
 
 ## Console Commands
+
 This mod adds some console commands to use with the SMAPI console. This can be useful in case a emote gets stuck playing or to stop playing emotes.
 
 The commands are the following:
   
-| Command 				| Parameters 							              | Action
-| --------------------- | --------------------------------------------------- | ----------------------------------------
-| `emote <value>`		| `<value>`: A integer representing the animation id. | Play the emote with the passed id
-| `stop_emote` 		    | None				                                  | Stop any playing emote
-| `stop_all_emotes` 	| None		                                          | Stop any playing emote by other players
+| Command                       | Parameters                                                                                            | Action                                                    |
+| ----------------------------- | ----------------------------------------------------------------------------------------------------- | --------------------------------------------------------- |
+| `emote <value>`               | `<value>`: A integer representing the animation id.                                                   | Play the emote with the given id                          |
+| `emote_npc <value> <npcName>` | `<value>`: A integer representing the animation id.__`<npcName>`: A string representing the npc name. | Force a npc to play the emote animation with the given id |
+| `stop_emote`                  | None                                                                                                  | Stop any playing emote                                    |
+| `stop_all_emotes`             | None                                                                                                  | Stop any playing emote by other players                   |
+| `multiplayer_emotes`          | None                                                                                                  | List all the players that have this mod installed         |
